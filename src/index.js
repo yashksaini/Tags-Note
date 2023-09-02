48e7f08f96903a26dac44d3793dfab14b17ef84c
Yash Kumar Saini <101442489+yashksaini@users.noreply.github.com>
2023-09-02 19:41:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import UserContextProvider from "./UserContext";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASb6McR0qmzANIee7CXICfjt0y7jT84mY",
+  apiKey: "apiKey",
   authDomain: "tags-note.firebaseapp.com",
   projectId: "tags-note",
   storageBucket: "tags-note.appspot.com",
-  messagingSenderId: "103734559495",
-  appId: "1:103734559495:web:42d342dcbe8258320a7ba2",
-  measurementId: "G-054N5P9CYW",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId",
+  measurementId: "measurementId",
 };
 // Initialize Firebase
 initializeApp(firebaseConfig);
